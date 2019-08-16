87bb1d5c057535e68fec0da2ee976576a63ab37f
Natalie Weizenbaum <nweiz@google.com>
2019-08-16 13:05:24 -0700

    Style tweaks

--- sass/sass_wrapper.js
+++ sass/sass_wrapper.js
@@ -9,6 +9,7 @@
  * It can also be invoked with an argument file to run once and exit.
  */
 "use strict";
+
 const {debug, runAsWorker, runWorkerLoop} = require('@bazel/worker');
 const sass = require('sass');
 const fs = require('fs');
@@ -21,12 +22,11 @@ if (runAsWorker(args)) {
   // is waiting for async callbacks from node.
 } else {
   debug('Running a single build...');
-  if (args.length === 0) {
-    throw new Error('Not enough arguments');
-  }
+  if (args.length === 0) throw new Error('Not enough arguments');
   if (args.length !== 1) {
     throw new Error('Expected one argument: path to flagfile');
   }
+
   // Bazel worker protocol expects the only arg to be @<path_to_flagfile>.
   // When we are running a single build, we remove the @ prefix and read the list 
   // of actual arguments line by line.
@@ -34,4 +34,5 @@ if (runAsWorker(args)) {
   const configContent = fs.readFileSync(configFile, 'utf8').trim();
   sass.run_(configContent.split('\n'));
 }
+
 process.exitCode = 0;
